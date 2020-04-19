8346fc0c7740db13ba6b46f711450d6e1cb3b726
Zachary Spar <zachspar@gmail.com>
2020-04-19 16:29:24 -0400

    update  app and serve static

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='/static')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('home/index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
