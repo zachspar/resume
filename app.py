ab66f2b1c47a73fefa2f1db7730abae354c0a716
Zachary Spar <zachspar@gmail.com>
2023-03-09 00:34:48 -0500

    lint and update ci actions

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
+import tomllib
 
 from flask_sslify import SSLify
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+with open("config.toml") as f:
+    template_config = tomllib.load(f)
+
 if "DYNO" in os.environ:
     sslify = SSLify(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('home/index.html')
+    return render_template("home/index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
