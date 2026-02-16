62c0e3b3da160fe686cda592ca841048ed661df4
Zachary Spar <41600414+zachspar@users.noreply.github.com>
2026-02-16 12:54:25 -0500

    fix: deployment probes (#17)

--- app.py
+++ app.py
@@ -24,5 +24,10 @@ def index():
     return render_template("home/index_template.html", **template_config)
 
 
+@app.get("/healthz")
+def health():
+    return {"status": "ok"}
+
+
 if __name__ == "__main__":
     app.run()
